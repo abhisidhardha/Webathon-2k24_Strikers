182a7315bbb6f4e0cfb4040ddfb4df32b22839ed
abhisidhardha <kocharlaabhi1289@gmail.com>
2024-03-22 12:51:32 +0530

    devhive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Root from './components/Root';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,61 @@
+import "./Root.css"
 function Root(){
     return(
         <div>
         <div className="">
-            <div className="navbar">
-                <h1>Home</h1>
-                <ul>
-                    <li>Gallery</li>
-                    <li>Resources</li>
-                    <li>Mentorship</li>
-                    <li>Sign-In/Login</li>
+            <div className="navbar sticky-top">
+                <h1 className="p-2">DevHive</h1>
+                <ul className="d-flex list-unstyled">
+                    <li className="p-3 rounded">Gallery</li>
+                    <li className="p-3 rounded">Resources</li>
+                    <li className="p-3 rounded">Mentorship</li>
+                    <li className="p-3 rounded" >Sign-In/Login</li>
                 </ul>
             </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <var><div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            </var>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+            <div className="body">
+                <h1>Welcome To </h1>
+            </div>
+
         </div>
     </div>
     )
