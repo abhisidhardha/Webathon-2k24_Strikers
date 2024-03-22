62a863339d83c910d921fa4d3a3da5bcf9dac4a5
abhisidhardha <kocharlaabhi1289@gmail.com>
2024-03-22 11:51:51 +0530

    feature signup working

---
+++ src/components/Root.js
@@ -0,0 +1,19 @@
+function Root(){
+    return(
+        <div>
+        <div className="">
+            <div className="navbar">
+                <h1>Home</h1>
+                <ul>
+                    <li>Gallery</li>
+                    <li>Resources</li>
+                    <li>Mentorship</li>
+                    <li>Sign-In/Login</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    )
+}
+    
+export default Root ;
