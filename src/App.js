bc2a699e9fb21215904c98b2beec23f2224ac847
abhisidhardha <kocharlaabhi1289@gmail.com>
2024-03-23 10:47:53 +0530

    devHive

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Root from './components/Root';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{
+    document.title="DevHive"
+  },[]);
   return (
     <div className="App">
       <Root/>
