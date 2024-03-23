d820ea6be414b7e5a4469c69a2726d7e6c372ffe
abhisidhardha <kocharlaabhi1289@gmail.com>
2024-03-23 11:02:31 +0530

    devHive

--- src/components/Proceed.js
+++ src/components/Proceed.js
@@ -4,11 +4,11 @@ import Register from "./Register";
 function Proceed(){
     return(
         <div className="h-100 d-flex flex-column justify-content-center">
-            <div className="d-flex text-center text-white justify-content-center ">
-                <NavLink className="nav-link mx-3"  to='register'>
+            <div className="d-flex text-center justify-content-center ">
+                <NavLink className="nav-link mx-3 text-info"  to='register'>
                         Sign-In
                 </NavLink>
-                <NavLink className="nav-link mx-3"  to='login'>
+                <NavLink className="nav-link mx-3 text-info"  to='login'>
                         Login
                 </NavLink>
             </div>
